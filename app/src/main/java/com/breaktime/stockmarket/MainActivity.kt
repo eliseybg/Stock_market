ac4903a56314e204c732ef1bd11d4e77ad4708ee
Elisey Budnitsky <bgs6544@gmail.com>
2022-05-21 05:56:03 +0300

    init commit

---
+++ app/src/main/java/com/breaktime/stockmarket/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.breaktime.stockmarket
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import com.breaktime.stockmarket.ui.theme.StockMarketTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            StockMarketTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
