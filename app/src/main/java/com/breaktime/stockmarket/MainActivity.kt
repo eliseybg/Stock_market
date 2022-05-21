d300f972d1715eb5d1436fcf8ea2e1001beaae92
Elisey Budnitsky <bgs6544@gmail.com>
2022-05-21 08:35:15 +0300

    feat: add start navigation

--- app/src/main/java/com/breaktime/stockmarket/MainActivity.kt
+++ app/src/main/java/com/breaktime/stockmarket/MainActivity.kt
@@ -7,7 +7,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
+import com.breaktime.stockmarket.presentation.company_listings.NavGraphs
 import com.breaktime.stockmarket.ui.theme.StockMarketTheme
+import com.ramcosta.composedestinations.DestinationsNavHost
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -20,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-
+                    DestinationsNavHost(navGraph = NavGraphs.root)
                 }
             }
         }
