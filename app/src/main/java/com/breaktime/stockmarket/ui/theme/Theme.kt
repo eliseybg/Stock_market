4b18941139a4de342ebad4dedee99d80265ed8fb
Elisey Budnitsky <bgs6544@gmail.com>
2022-05-21 09:17:50 +0300

    feat: change app theme

--- app/src/main/java/com/breaktime/stockmarket/ui/theme/Theme.kt
+++ app/src/main/java/com/breaktime/stockmarket/ui/theme/Theme.kt
@@ -5,11 +5,14 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = Color.Green,
+    onPrimary = Color.DarkGray,
+    background = DarkBlue,
+    onBackground = TextWhite
+
 )
 
 private val LightColorPalette = lightColors(
@@ -29,7 +32,7 @@ private val LightColorPalette = lightColors(
 
 @Composable
 fun StockMarketTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
-    val colors = if (darkTheme) {
+    val colors = if (true) {
         DarkColorPalette
     } else {
         LightColorPalette
