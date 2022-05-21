cf4917b333d3e0a96776e194a4abd6d0af26fe38
Elisey Budnitsky <bgs6544@gmail.com>
2022-05-21 12:48:32 +0300

    feat: add navigation

--- app/src/main/java/com/breaktime/stockmarket/presentation/company_listings/CompanyListingsScreen.kt
+++ app/src/main/java/com/breaktime/stockmarket/presentation/company_listings/CompanyListingsScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.breaktime.stockmarket.presentation.destinations.CompanyInfoScreenDestination
 import com.google.accompanist.swiperefresh.SwipeRefresh
 import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
 import com.ramcosta.composedestinations.annotation.Destination
@@ -60,7 +61,7 @@ fun CompanyListingsScreen(
                         modifier = Modifier
                             .fillMaxWidth()
                             .clickable {
-                                // TODO: navigate to details screen
+                                navigator.navigate(CompanyInfoScreenDestination(company.symbol))
                             }
                             .padding(16.dp)
                     )
